192d5e8fff217c1a1c0a5a6d6cf96c1d41435c27
Rusira Indrachapa <indrachaparusira12@gmail.com>
2026-06-11 12:53:30 +0530

    add  dark mode

--- script.js
+++ script.js
@@ -508,8 +508,17 @@ function removeActive(x) {
   }
 }
 
+const themeBtn = document.getElementById("themeBtn");
 
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
 
+    if(document.body.classList.contains("dark-mode")){
+        themeBtn.innerHTML = "🔅";
+    }else{
+        themeBtn.innerHTML = "⚫";
+    }
+});
 
 
 
